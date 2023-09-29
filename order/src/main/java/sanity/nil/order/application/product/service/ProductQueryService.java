ccca620b61c391b750ae07ff2dbd2b8b4040ce01
Yuno_win <d.2510086@gmail.com>
2023-09-30 00:51:56 +0100

    refactor -> deleted unnecessary interfaces in product, changed di

---
+++ order/src/main/java/sanity/nil/order/application/product/service/ProductQueryService.java
@@ -0,0 +1,14 @@
+package sanity.nil.order.application.product.service;
+
+import lombok.RequiredArgsConstructor;
+import sanity.nil.order.application.product.query.GetAllProductsQuery;
+import sanity.nil.order.application.product.query.GetProductByIdQuery;
+import sanity.nil.order.application.product.query.GetProductsByNameQuery;
+
+@RequiredArgsConstructor
+public class ProductQueryService {
+
+    public final GetAllProductsQuery getAllProductsQuery;
+    public final GetProductByIdQuery getProductByIdQuery;
+    public final GetProductsByNameQuery getProductsByNameQuery;
+}

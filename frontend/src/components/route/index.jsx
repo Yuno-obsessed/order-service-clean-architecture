4d89e420eae7f27706d160cf451c8dec73d2e433
Yuno_win <d.2510086@gmail.com>
2023-12-11 13:17:01 +0100

    Resolving conflicts

---
+++ frontend/src/components/route/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss";
+
+export const Route = () => {
+    return (
+        <div className={styles.root}>
+            <p>dsads > dsadsa</p>
+        </div>
+    );
+};
